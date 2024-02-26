91df4ef80da65c112408052c32d4f53a6b0d3fbb
Victor Galvez <vhgalvez@gmail.com>
2024-02-26 06:31:34 +0100

    Update costo_operativo_servidor_historico.sh

--- costo_operativo_servidor_historico.sh
+++ costo_operativo_servidor_historico.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Comprobación para evitar la ejecución como root
+# Evita la ejecución como root
 if [ "$(id -u)" -eq 0 ]; then
     echo "Este script no debe ser ejecutado como root. Por favor, ejecute como un usuario regular."
     exit 1
@@ -10,19 +10,20 @@ set -e
 set -o errexit
 set -o nounset
 
+# Variables globales
 fecha_hora=$(date "+%Y-%m-%d_%H-%M-%S")
 directorio_salida="/home/$USER/costo_operativo_servidor"
 archivo_salida="${directorio_salida}/salida_consumo_temperatura_${fecha_hora}.txt"
 costo_kwh=0.189
-consumo_watts=125 # Ajusta este valor según tu sistema
+consumo_watts=125 # Ajuste este valor según su sistema
 consumo_kwh_por_hora=$(echo "scale=3; $consumo_watts / 1000" | bc)
 
-# Función para crear el directorio de salida si no existe
+# Crea el directorio de salida si no existe
 crear_directorio() {
     mkdir -p "$directorio_salida"
 }
 
-# Función para calcular el costo en base a las horas de funcionamiento
+# Calcula el costo en base a las horas de funcionamiento
 calcular_costo() {
     local horas=$1
     local consumo_kwh=$(echo "$horas * $consumo_kwh_por_hora" | bc)
@@ -30,17 +31,18 @@ calcular_costo() {
     echo $(printf "%.2f" $costo)
 }
 
-# Función para procesar cada línea de "last -F reboot"
+# Procesa cada línea de "last reboot"
 procesar_linea() {
-    local linea=$1
-    local fecha=$(echo $linea | awk '{print $5, $6, $7}')
-    local inicio=$(echo $linea | awk '{print $8}')
-    local fin=$(echo $linea | awk '{print $9}')
+    local fecha=$(echo $1 | awk '{print $5, $6, $7}')
+    local inicio=$(echo $1 | awk '{print $8}')
+    local fin=$(echo $1 | awk '{print $9}')
 
+    # Ajusta para entradas "still running"
     if [[ "$fin" == *"still"* ]]; then
         fin=$(date "+%H:%M")
     fi
 
+    # Calcula segundos de inicio y fin
     local inicio_sec=$(date -d "$fecha $inicio" +%s)
     local fin_sec=$(date -d "$fecha $fin" +%s)
     local duracion_sec=$(($fin_sec-$inicio_sec))
@@ -51,11 +53,11 @@ procesar_linea() {
     echo "$fecha: $horas horas, Costo: €$costo" >> "$archivo_salida"
 }
 
-# Generar el reporte
+# Genera el reporte
 generar_reporte() {
     echo "Reporte de Consumo Energético - $fecha_hora" > "$archivo_salida"
     echo "-------------------------------------------" >> "$archivo_salida"
-    last -F reboot | while read line; do
+    last -F reboot | grep -v wtmp | while read line; do
         procesar_linea "$line"
     done
 }
